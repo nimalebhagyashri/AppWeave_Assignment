e6f79d13b6ee25a33282688e0b1f5b264312d23a
nimalebhagyashri <117168975+nimalebhagyashri@users.noreply.github.com>
2024-05-31 00:43:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
 
-// Serve static files from the 'public' folder within the project directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, '../../public')));
 
 app.get('/', (req, res) => {
-  // Serve the index.html file from the 'public' folder within the project directory
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
+module.exports.handler = serverless(app);
